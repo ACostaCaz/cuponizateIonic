b70de46b16beb9a424817f63729f0cf83a122085
danieldiaz98 <danieldiaz.g98@gmail.com>
2022-05-19 22:06:23 +0100

    index partialy done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { BusinessProfileComponent } from './business-profile/business-profile.component';
 import { BusinessCouponsComponent } from './business-coupons/business-coupons.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [AppComponent,
@@ -22,7 +23,8 @@ import { BusinessCouponsComponent } from './business-coupons/business-coupons.co
     LoginComponent,
     RegisterComponent,
     BusinessProfileComponent,
-    BusinessCouponsComponent],
+    BusinessCouponsComponent,
+    IndexComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
